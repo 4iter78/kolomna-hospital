7e7f5fccacb583a7ac955e87f7fd8bf6808713d7
4iter78 <meg0697380@mail.ru>
2026-04-17 00:29:14 +0300

    create table drugs,create table treatment_types,fill table treatment_types, create table user_roles,fill table user_roles

---
+++ alembic/versions/2026_04_17_0010-666382c79b82_create_table_user_roles.py
@@ -0,0 +1,32 @@
+"""create table user_roles
+
+Revision ID: 666382c79b82
+Revises: 70101c54188a
+Create Date: 2026-04-17 00:10:21.906115
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '666382c79b82'
+down_revision: Union[str, Sequence[str], None] = '70101c54188a'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute('''
+       CREATE TABLE user_roles (
+            id   SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL
+        );
+    ''')
+    pass
+
+def downgrade() -> None:
+    op.execute('DROP TABLE IF EXISTS user_roles;')
+    pass
---
+++ alembic/versions/2026_04_17_0016-604b392359df_fill_table_user_roles.py
@@ -0,0 +1,43 @@
+"""fill table user_roles
+
+Revision ID: 604b392359df
+Revises: 666382c79b82
+Create Date: 2026-04-17 00:16:10.879202
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '604b392359df'
+down_revision: Union[str, Sequence[str], None] = '666382c79b82'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute('''
+        INSERT INTO user_roles (name) VALUES
+            ('Врач'),
+            ('Регистратор'),
+            ('Работник ИТ'),
+            ('Аспирант'),
+            ('Обслуживающий персонал'),
+            ('Гость'),
+            ('Администратор');
+    ''')
+    pass
+def downgrade() -> None:
+    op.execute('''
+        DELETE FROM treatment_types WHERE name IN ('Врач',
+            'Регистратор',
+            'Работник ИТ',
+            'Аспирант',
+            'Обслуживающий персонал',
+            'Гость',
+            'Администратор');
+    ''')
+    pass
