f0ecb4fc123b97b1bfaf91f90367ebf29494854c
4iter78 <meg0697380@mail.ru>
2026-04-18 13:00:20 +0300

    create table drugs,create table treatment_types,fill table treatment_types, create table user_roles,fill table user_roles

---
+++ alembic/versions/2026_04_18_1253-f7533eca5af3_create_table_room_type.py
@@ -0,0 +1,31 @@
+"""create table room_type
+
+Revision ID: f7533eca5af3
+Revises: 604b392359df
+Create Date: 2026-04-18 12:53:25.654281
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'f7533eca5af3'
+down_revision: Union[str, Sequence[str], None] = '604b392359df'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute('''
+        CREATE TABLE room_type (
+            id   SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL
+        );
+    ''')
+    pass
+def downgrade() -> None:
+    op.execute('DROP TABLE IF EXISTS room_type;')
+    pass
