35d308c1e4664524b4a61cdd2b6f666eca3ff89c
4iter78 <meg0697380@mail.ru>
2026-04-18 13:27:35 +0300

    create table special_type

---
+++ alembic/versions/2026_04_18_1317-af25a62c983a_create_table_special_type.py
@@ -0,0 +1,32 @@
+"""create table special_type
+
+Revision ID: af25a62c983a
+Revises: 2bd36d738348
+Create Date: 2026-04-18 13:17:24.953667
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'af25a62c983a'
+down_revision: Union[str, Sequence[str], None] = '2bd36d738348'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute('''
+        CREATE TABLE special_type (
+            id   SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL
+        );
+    ''')
+    pass
+
+def downgrade() -> None:
+    op.execute('DROP TABLE IF EXISTS special_type;')
+    pass
