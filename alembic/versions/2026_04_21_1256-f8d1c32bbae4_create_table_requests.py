b0e229a27526d71af7884832cc32cf67315151bd
4iter78 <meg0697380@mail.ru>
2026-04-21 12:57:59 +0300

    create table requests

---
+++ alembic/versions/2026_04_21_1256-f8d1c32bbae4_create_table_requests.py
@@ -0,0 +1,33 @@
+"""create table requests
+
+Revision ID: f8d1c32bbae4
+Revises: 121121a9487a
+Create Date: 2026-04-21 12:56:24.695589
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'f8d1c32bbae4'
+down_revision: Union[str, Sequence[str], None] = '121121a9487a'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute('''
+        CREATE TABLE requests (
+            id           SERIAL PRIMARY KEY,
+            request_file TEXT
+        );
+    ''')
+    pass
+
+
+def downgrade() -> None:
+    op.execute('DROP TABLE IF EXISTS requests;')
+    pass
