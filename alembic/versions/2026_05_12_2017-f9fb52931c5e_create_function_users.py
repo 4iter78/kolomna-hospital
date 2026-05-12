8010a9633aa72b64d1dc37ed4f44bf6b6c7ad529
4iter78 <meg0697380@mail.ru>
2026-05-12 20:22:52 +0300

    create function trg_users_hash_password

---
+++ alembic/versions/2026_05_12_2017-f9fb52931c5e_create_function_users.py
@@ -0,0 +1,45 @@
+"""create function users
+
+Revision ID: f9fb52931c5e
+Revises: 7beb1bbe67e7
+Create Date: 2026-05-12 20:17:57.967093
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import text
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'f9fb52931c5e'
+down_revision: Union[str, Sequence[str], None] = '7beb1bbe67e7'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute('''
+    -- Функция триггера для работы с паролем
+    CREATE OR REPLACE FUNCTION trg_users_hash_password()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS $$
+    BEGIN
+        -- Если поле password передано и не пустое — считаем хеш
+        IF NEW.password IS NOT NULL AND NEW.password <> '' THEN
+            NEW.hash_password := encode(digest(NEW.password, 'sha256'), 'hex');
+        END IF;
+        -- Очищаем password — значение не сохраняется в таблице
+        NEW.password := NULL;
+        RETURN NEW;
+    END;
+    $$;
+    ''')
+
+
+def downgrade() -> None:
+    op.execute('''
+    DROP FUNCTION IF EXISTS trg_users_hash_password();
+    ''')
