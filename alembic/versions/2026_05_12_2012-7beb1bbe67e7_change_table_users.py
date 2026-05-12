dbd65897e7a84bb4386d5dad8bd9e18a8e87ed23
4iter78 <meg0697380@mail.ru>
2026-05-12 20:16:37 +0300

    change table users

---
+++ alembic/versions/2026_05_12_2012-7beb1bbe67e7_change_table_users.py
@@ -0,0 +1,36 @@
+"""change table users
+
+Revision ID: 7beb1bbe67e7
+Revises: 8880a4462cba
+Create Date: 2026-05-12 20:12:47.327126
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = '7beb1bbe67e7'
+down_revision: Union[str, Sequence[str], None] = '8880a4462cba'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute('''
+    ALTER TABLE users
+        ADD COLUMN IF NOT EXISTS login VARCHAR(100),
+        ADD COLUMN IF NOT EXISTS password VARCHAR(255),
+        ADD COLUMN IF NOT EXISTS hash_password VARCHAR(64),
+        ADD CONSTRAINT uq_users_login UNIQUE (login);
+    ''')
+
+
+def downgrade() -> None:
+    op.execute('''
+    ALTER TABLE users
+        DROP CONSTRAINT IF EXISTS uq_users_login,
+        DROP COLUMN IF EXISTS login,
+        DROP COLUMN IF EXISTS password,
+        DROP COLUMN IF EXISTS hash_password;
+    ''')
