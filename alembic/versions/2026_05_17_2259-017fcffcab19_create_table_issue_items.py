a7b917780ba053f1767a9370614176c30dc2bebd
4iter78 <meg0697380@mail.ru>
2026-05-17 23:00:09 +0300

    create table issue_items

---
+++ alembic/versions/2026_05_17_2259-017fcffcab19_create_table_issue_items.py
@@ -0,0 +1,40 @@
+"""create table issue_items
+
+Revision ID: 017fcffcab19
+Revises: 850af15b8073
+Create Date: 2026-05-17 22:59:33.969364
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import text
+
+
+# revision identifiers, used by Alembic.
+revision: str = '017fcffcab19'
+down_revision: Union[str, Sequence[str], None] = '850af15b8073'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.execute('''
+        CREATE TABLE issue_items (
+            id               SERIAL PRIMARY KEY,
+            material_issue_id INT NOT NULL REFERENCES material_issues(id),
+            medical_material_id INT NOT NULL REFERENCES medical_materials(id),
+            quantity         INT NOT NULL CHECK (quantity > 0)
+        );
+    ''')
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.execute('''
+        DROP TABLE IF EXISTS issue_items;
+    ''')
+    pass
